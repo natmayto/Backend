d42f65d416de3a2c1e82b0a66a21a80103e8ffd5
Natalia Maya <natmaya98@gmail.com>
2023-07-19 10:27:16 -0500

    tdg

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,7 +2,7 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { getUsuarios, crearUsuario, actualizarUsuario, borrarUsuario } = require('../controllers/UsuarioController');
+const { getUsuarios, crearUsuario, actualizarUsuario, borrarUsuario } = require('../controllers/usuarioController');
 const { validarJWT } = require('../middlewares/validar-jwt');
 const { validarCampos } = require('../middlewares/validarcampos');
 
